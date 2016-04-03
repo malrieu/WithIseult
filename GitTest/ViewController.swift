45b276c3fbbcd205cfc27a2fd7666ca7b0471a9f
Jean Pierre Malrieu <jp.malrieu@gmail.com>
2016-04-03 17:24:49 +0200

    second commit

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Coucou Iseult")
+        print("Coucou Iseult bienvenue sur github")
     }
 
     override func didReceiveMemoryWarning() {
