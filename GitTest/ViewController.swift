ff4c9954152de7e1d0ebfbf6795bca83f128d0d5
Jean Pierre Malrieu <jp.malrieu@gmail.com>
2016-04-03 17:04:07 +0200

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Jean-Pierre Malrieu on 03/04/2016.
+//  Copyright © 2016 Educlab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
